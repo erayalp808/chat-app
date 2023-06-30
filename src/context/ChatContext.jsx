e8dee92f34c6912e99f33769034bed497dddbff0
Eray Alp <erayalp.e@gmail.com>
2023-06-30 11:56:54 +0300

    useChat function added

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 
 const ChatContext = createContext();
@@ -14,4 +14,4 @@ export const ChatProvider = ({children}) => {
     return <ChatContext.Provider value={values}>{children}</ChatContext.Provider>
 }
 
-export default ChatContext;
+export const useChat = () => useContext(ChatContext);
