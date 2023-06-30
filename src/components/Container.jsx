9a199220c076e00e1f75977448e7d24071b04ce4
Eray Alp <erayalp.e@gmail.com>
2023-06-30 11:50:58 +0300

    ChatList and ChatForm components imported

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ChatList from './ChatList'
+import ChatForm from './ChatForm'
 
 function Container() {
   return (
-    <div>Container</div>
+    <div>
+      <ChatList />
+      <ChatForm />
+    </div>
   )
 }
 
