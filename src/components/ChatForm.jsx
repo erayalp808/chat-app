326bae0f50b970b363200f6a7edd162c07293455
Eray Alp <erayalp.e@gmail.com>
2023-06-30 12:33:43 +0300

    form handling completed

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
+import styles from './styles.module.css'
 
 function ChatForm() {
+  const [message, setMessages] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setMessages("")
+  }
+
   return (
-    <div>ChatForm</div>
+    <div className={styles.form}>
+      <form onSubmit={handleSubmit}>
+        <input 
+        className={styles.textInput}
+        value={message}
+        onChange={(e) => setMessages(e.target.value)}
+        />
+      </form>
+    </div>
   )
 }
 
