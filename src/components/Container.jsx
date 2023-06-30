94bef04c81224099bd673c8b362fe01e94cab701
Eray Alp <erayalp.e@gmail.com>
2023-06-30 12:33:54 +0300

    container css added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ChatList from './ChatList'
 import ChatForm from './ChatForm'
+import styles from '../components/styles.module.css'
 
 function Container() {
   return (
-    <div>
+    <div className={styles.container}>
       <ChatList />
       <ChatForm />
     </div>
