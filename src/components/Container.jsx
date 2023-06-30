236c14f146dd3dc2971b34fe05dcdff81829e0f8
Eray Alp <erayalp.e@gmail.com>
2023-06-30 16:16:44 +0300

    messages sent to backend

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {useEffect} from 'react'
 import ChatList from './ChatList'
 import ChatForm from './ChatForm'
 import styles from '../components/styles.module.css'
+import { init, subscribeChat } from '../socketApi'
+import {useChat} from '../context/ChatContext'
 
 function Container() {
+  const { messages, setMessages } = useChat();
+
+  useEffect(() => {
+    init()
+
+    subscribeChat((message) => {
+      setMessages((prevState) => [...prevState, {message}])
+    })
+  }, [])
+
   return (
     <div className={styles.container}>
       <ChatList />
